7dcbb881c0f654f3cbe0872cf324d60948901344
jorgelemoss <153878466+jorgelemoss@users.noreply.github.com>
2024-08-07 14:45:20 -0300

    Update routes.js

--- backend/src/http/controllers/user/routes.js
+++ backend/src/http/controllers/user/routes.js
@@ -4,13 +4,14 @@ import {
     AuthenticateController,
     ProfileController
 } from './index.js'
+import { VerifyJWT } from '../../middlewares/jwt/VerifyJWT.js'
 
 const router = express.Router()
 
 router.post('/user', RegisterController) // Register a new User
 router.post('/auth/user', AuthenticateController) // Authenticate a user
 
-router.get('/user/profile', ProfileController) // Get User Profile (User should is authenticated)
+router.get('/user/profile', [VerifyJWT], ProfileController) // Get User Profile (User should is authenticated)
 
 export {
     router as UserRoutes //Rename route to UserRoutes and export it (reference: app.js)
